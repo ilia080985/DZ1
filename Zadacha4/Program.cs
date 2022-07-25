b50fea4ffb73eacc6298f98d3de49e8e03957869
Ilya <ilia080985@gmail.com>
2022-07-25 18:42:40 +0500

    Zadacha4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу
+// которая принимает на
+//вход три числа и выдаёт максимальное из этих чисел.
+
+double a = 200, b = 100, c = 150;
+if (a > b && a > c) 
+Console.WriteLine("Nubmer Max = " + a);
+else 
+if (b > c) 
+Console.WriteLine("Number Max = " + b);
+else 
+Console.WriteLine("Number Max = " + c); Console.ReadLine();
